12b6431e68d2f1d58029ffc7da8da8760fd4b200
Shanid Sajjatuz Islam <shanidsajjatuz@gmail.com>
2025-11-15 05:12:59 +0600

    Added main application structure with all sections

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import { Navbar } from './components/Navigation/Navbar';
+import { Hero } from './components/Hero/Hero';
+import { AboutSection } from './components/About/AboutSection';
+import { DomainSection } from './components/Domains/DomainSection';
+import { ContactSection } from './components/Contact/ContactSection';
+import { Footer } from './components/Footer/Footer';
+import { ScrollToTop } from './components/shared/ScrollToTop';
 
 function App() {
   return (
-    <div className="min-h-screen bg-background-primary text-gold-light">
-      <h1 className="text-4xl font-heading text-center py-20">
-        Yggrasoft Labs
-      </h1>
-      <p className="text-center text-xl">
-        World Tree of Innovation - Coming Soon
-      </p>
+    <div className="relative min-h-screen bg-background-primary">
+      <Navbar />
+      <main className="relative z-10">
+        <Hero />
+        <AboutSection />
+        <DomainSection />
+        <ContactSection />
+      </main>
+      <Footer />
+      <ScrollToTop />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
